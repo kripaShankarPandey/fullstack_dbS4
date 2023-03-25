993b09a85d07e0ea13b37ce783a3d7cc9e4ae559
kripa Shankar Pandey <kripapandey302@gmail.com>
2023-03-25 19:35:21 +0530

    dd

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 const main = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/crudaap");
+    await mongoose.connect(process.env.URL);
     console.log("server is connected to Database sucessfully");
   } catch (err) {
     console.log("Can't connected to DB");
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const main = require("./db");
 const cors = require("cors");
 app.use(cors());
@@ -8,6 +9,6 @@ const { notesRoute } = require("./routes/notes.routes");
 app.use(express.json());
 app.use("/user", userRoute);
 app.use("/notes", notesRoute);
-app.listen(8000, () => {
-  console.log("Server is running on 8000");
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on ${process.env.PORT}`);
 });
